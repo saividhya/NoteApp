79131fa35e7bd713957c1a2dc51c1da579c84b42
kselladu@asu.edu <kselladu@asu.edu>
2017-11-12 02:37:08 -0700

    dashboard page in progress

---
+++ noteapp-ui/src/components/explore.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+
+class Explore extends React.Component {
+  render () {
+    return (
+      <div>
+      <h1>Explore</h1>
+      </div>
+    )
+  }
+}
+
+export default Explore;
--- noteapp-ui/src/components/header.js
+++ noteapp-ui/src/components/header.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-export default class Header extends React.Component {
+export default class Example extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      isOpen: false
+      collapsed: true
     };
   }
-  toggle() {
+
+  toggleNavbar() {
     this.setState({
-      isOpen: !this.state.isOpen
+      collapsed: !this.state.collapsed
     });
   }
   render() {
+
+    //#8e36f4
     return (
       <div>
-        <Navbar color="dark" dark expand>
-          <NavbarBrand href="/">NoteApp</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Dashboard</NavLink>
-              </NavItem>
-            </Nav>
+        <Navbar style={{backgroundColor: '#8A2BE2'}} dark toggleable>
+          <NavbarBrand href="/" className="mr-auto">NoteApp</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Dashboard</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/explore">Explore</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/notification">Notification</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Profile">Profile</NavLink>
+            </NavItem>
+          </Nav>
           </Collapse>
         </Navbar>
       </div>
