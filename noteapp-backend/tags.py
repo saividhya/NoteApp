205633dbd0bd65c9daa708fc335b33e4520e78f2
Sai Pranav <rsaipranav92@gmail.com>
2017-11-27 00:52:18 -0700

    removed auth for tags

--- noteapp-backend/tags.py
+++ noteapp-backend/tags.py
@@ -2,15 +2,13 @@ from flask import Flask, request, session, abort, jsonify
 from models import *
 
 def getTags():
-    if 'user' not in session:
-        abort(403)
-    userEmail = session['user']['email']
     page = "user"
     if request.args.get('page') is not None:
         if request.args.get('page').lower() == "general": page = "general"
     tag_freqs = None
 
     if page == "user":
+        userEmail = session['user']['email']
         tag_freqs = Note.objects(contributors__in=[userEmail]).item_frequencies('tags', normalize=True)
     elif page == "general":
         tag_freqs = Note.objects.item_frequencies('tags', normalize=True)
